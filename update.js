724bd61c5891663c2b2ce9e4d4174d4885aef341
Salvatore <salvatpalvat@gmail.com>
2016-12-05 18:57:12 +0000

    Update a document

--- update.js
+++ update.js
@@ -1,14 +1,5 @@
 /**
         Update a document
-            Reqs:
-                1) Update a document in users collection
-                2) Database name will be passed as first command-line arg
-                3) Want to change Tina's age from 30 to 40
-                    +update()
-                        -First argument is query
-                            +The conditional statement that filters the documents
-                        -Second argument is an object of the properties to update
-                            +IMPORTANT: Without using $set property, the document would be updated by the object passed as the second argument
 **/
 
 const mongo = require('mongodb').MongoClient
