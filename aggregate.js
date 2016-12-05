24b1d2bd5549b99d385e3f8ee7e8e1ecc33aa9ce
Salvatore <salvatpalvat@gmail.com>
2016-12-05 22:51:09 +0000

    Find average price for all documents that meet a certain criteria

--- aggregate.js
+++ aggregate.js
@@ -1,22 +1,10 @@
 /**
         Find average price for all documents that meet a certain criteria
-            1) Calculate average price for all docs in prices collection
-                +Uses aggregate()
-                    -Takes an array of objects as first argument
-                        +Each object represents a pipeline stage
-                            -$match: similar to query object
-                            -$group: allows operations to be run on certain properties
-                                +Total is a property that will be assigned aggregation output
-                                +Object bound to $group property shows what fields you want to show  
-                                +Only need to sum the prices
-            2) Only done for docs where size is equal to first command-line arg
-            3) Print the average price rounded to 2 decimal places
 **/
 
 const mongo = require('mongodb').MongoClient
 const url = 'mongodb://localhost:27017/learnyoumongo'
-const userSize = parseInt(process.argv[2])
-
+const userSize = process.argv[2]
 
 
 mongo.connect(url, (err, db) => {
@@ -24,15 +12,16 @@ mongo.connect(url, (err, db) => {
     //Find collection
     const collection = db.collection('prices')
     
-    //Counting number of documents meeting a certain criteria
+    //For all documents with a size field of a certain value, find the average of those documents' price field
     collection.aggregate([
         { $match: {size: userSize} },
-        {$group: { _id: "$_id", total: {$sum: '$prices'} }}
+        {$group: { _id: "total", total: {$avg: '$price'} }}
     ])
         .toArray((err, arr) => {
             if (err) throw err
             //Access value assigned to total property and round to 2 decimal places
             const roundedResult = Number(arr[0].total).toFixed(2)
             console.log(roundedResult)
+            db.close()
         })
 })
